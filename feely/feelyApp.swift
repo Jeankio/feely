67a0aac38fde10825224d86dd2b2c44c1dd0b0a9
Jean Karel <mailjeank@gmail.com>
2023-05-19 10:36:01 -0500

    Initial Commit

---
+++ feely/feelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  feelyApp.swift
+//  feely
+//
+//  Created by Jean Karel on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct feelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
